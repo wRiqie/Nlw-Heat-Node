cc0f30e91891e8f9635895622c724749917343c5
Henrique Moisés Fernandes <henriquegame512@outlook.com>
2022-05-07 23:40:13 -0300

    :sparkles: profile user and messages!

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastThreeMessagesController } from "./controllers/GetLastThreeMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
-router.post('/authenticate', new AuthenticateUserController().handle);
+router.post("/authenticate", new AuthenticateUserController().handle);
 
-export { router }
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
+
+router.get("/messages/lastThree", new GetLastThreeMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
+
+export { router };
